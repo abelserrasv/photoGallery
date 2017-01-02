f36673d396eb2f288f4548afc0433e3a3eb848e1
Abel Serra <abel.serra@jambit.com>
2017-01-02 18:00:31 +0100

    Added documentation.

--- photoGallery/ASPhotoGalleryCollectionViewController.swift
+++ photoGallery/ASPhotoGalleryCollectionViewController.swift
@@ -8,17 +8,32 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
+/**
+ 
+ - A UIViewController shouldn’t have more than 130 lines of code.
+ - Most of the code inside the view controller is related to its root UIView customization.
+ - It should be responsible for hooking up the controller with its root UIView, for example, calling a controller’s method on an IBAction.
+ - Things like UITableDataSource, UITableViewDelegate and other delegates shouldn’t be inside it. If they are, it’s impossible to test them.
+ - The data model (ASPhotoGalleryModel in this class) should be defined in one separated class.
+ 
+ */
 
 class ASPhotoGalleryCollectionViewController: UICollectionViewController {
 
     var galleryModel: ASPhotoGalleryModel?
     var collectionViewDataSource: ASPhotoGalleryCollectionViewDataSource?
     var collectionViewDelegate: ASPhotoGalleryCollectionViewDelegate?
+    var photosManager : ASPhotoManagement?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        galleryModel = ASPhotoGalleryModel()
+        
+        self.photosManager = ASPhotoManagement()
+        
+        // Dependency injection design pattern (constructor injection).
+        self.galleryModel = ASPhotoGalleryModel(self.photosManager!)
+        
         if let photoGalleryModel = galleryModel {
             collectionViewDataSource = ASPhotoGalleryCollectionViewDataSource(collectionView: self.collectionView!, galleryModel: photoGalleryModel)
             collectionViewDelegate = ASPhotoGalleryCollectionViewDelegate(collectionView: self.collectionView!, galleryModel: photoGalleryModel)
--- photoGallery/ASPhotoGalleryModel.swift
+++ photoGallery/ASPhotoGalleryModel.swift
@@ -9,14 +9,36 @@
 import UIKit
 import Photos
 
+/**
+ Understand the responsibility of each object. Once you know that, start thinking about the code. 
+ In this class I'm using the Single Responsability Principle and the dependency injection design pattern. I see specially important the DI pattern in terms of testability.
+ 
+ The Single Responsibility Principle. (SRP)
+ - A class should have only one reason to change.
+
+ Dependency injection offers a number of benefits, but some of the more important ones are:
+ 
+ - Clear declaration of dependencies It becomes obvious what an object needs in order to operate, and dangerous hidden dependencies (like globals) disappear.
+ - Easy customization When creating an object, it’s easy to customize parts of the object for specific scenarios.
+ - Clear ownership Particularly when using constructor injection, the object ownership rules are strictly enforced — helping to build a directed acyclic object graph.
+ - Testability More than anything else, dependency injection improves the testability of your objects. Because they can be created simply by filling in the initializer, no hidden dependencies need to be managed. Furthermore, it becomes simple to mock out the dependencies to focus your tests on the object being tested.
+ */
+
 class ASPhotoGalleryModel: NSObject {
-    var photosManager : ASPhotoManagement
+    var photosManager : ASPhotoManagement?
     
     override init() {
-        self.photosManager = ASPhotoManagement()
+    }
+    
+    // Dependencies Allocated During Initialization
+    // The photoManager is created before the gallery model and passed in
+    // as a parameter, and the caller can customize it if desired.
+    convenience init(_ photoManager : ASPhotoManagement) {
+        self.init()
+        self.photosManager = photoManager;
     }
     
     func getFetchResultsFromGallery () -> PHFetchResult<PHAsset> {
-        return photosManager.getPhotosFromGallery()
+        return self.photosManager!.getPhotosFromGallery()
     }
 }
