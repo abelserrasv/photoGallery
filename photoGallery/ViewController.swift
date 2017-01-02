fe00b36f107b9f559be29a444b14bbb5e39d0893
Abel Serra <abel.serra@jambit.com>
2017-01-02 14:58:08 +0100

    Initial Commit

---
+++ photoGallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  photoGallery
+//
+//  Created by Abel Serra on 02/01/17.
+//  Copyright © 2017 Abel Serra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
