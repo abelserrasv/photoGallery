9955b112fc63990db635421f39d8f21998776006
Abel Serra <abel.serra@jambit.com>
2017-01-02 16:47:30 +0100

    Added Collection View Controller in the storyboard and CollectionVC updated.

--- photoGallery/ASPhotoGalleryCollectionViewController.swift
+++ photoGallery/ASPhotoGalleryCollectionViewController.swift
@@ -12,68 +12,23 @@ private let reuseIdentifier = "Cell"
 
 class ASPhotoGalleryCollectionViewController: UICollectionViewController {
 
+    var galleryModel: ASPhotoGalleryModel?
+    var collectionViewDataSource: ASPhotoGalleryCollectionViewDataSource?
+    var collectionViewDelegate: ASPhotoGalleryCollectionViewDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
+        galleryModel = ASPhotoGalleryModel()
+        if let photoGalleryModel = galleryModel {
+            collectionViewDataSource = ASPhotoGalleryCollectionViewDataSource(collectionView: self.collectionView!, galleryModel: photoGalleryModel)
+            collectionViewDelegate = ASPhotoGalleryCollectionViewDelegate(collectionView: self.collectionView!, galleryModel: photoGalleryModel)
+        }
+        collectionView?.dataSource = collectionViewDataSource
+        collectionView?.delegate = collectionViewDelegate
     }
-
-    // MARK: UICollectionViewDataSource
-
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 0
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
     
-        // Configure the cell
-    
-        return cell
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        collectionView?.reloadData()
     }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
-
 }
